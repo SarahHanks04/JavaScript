6e95691df246822273fdfa9792c546423e9b06d0
SarahHanks04 <sarahhanks04@gmail.com>
2024-05-30 19:29:27 +0100

    first commit

---
+++ Maryland Cookies.js
@@ -0,0 +1,27 @@
+// HOW TO CHANGE BACKGROUND COLOR ONCLICK
+
+document.getElementById("Cookies1").addEventListener("click", function() {
+    document.body.style.backgroundColor = "#e60000";
+});
+
+document.getElementById("Cookies2").addEventListener("click", function() {
+    document.body.style.backgroundColor = "#9370DB";
+});
+
+document.getElementById("Cookies3").addEventListener("click", function() {
+    document.body.style.backgroundColor = "#4d2600";
+});
+
+document.getElementById("Cookies4").addEventListener("click", function() {
+    document.body.style.backgroundColor = "#00BFFF";
+}); 
+
+
+// SECOND METHOD ON HOW TO CHANGE BACKGROUND COLOR & IMAGE
+// let image = document.querySelector('#right');
+// const changeImg=(source)=>{
+//     image.src = source
+// }
+// function changeColor(color) {
+//     document.querySelector('.main').style.backgroundColor = color
+// }
